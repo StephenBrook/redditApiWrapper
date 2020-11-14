97c8a1d445f1ffe48cb5a17665c6e3bed67ef1df
Pataki István <pataki.istvan@e-vista.hu>
2020-11-14 17:25:29 +0100

    initial commit

---
+++ src/RedditApi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Istvanp\RedditApi;
+
+class RedditApi
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
